b930d11d174f8a9f43066e3130e1eb0feabc5167
iniyo <inizle35@gmail.com>
2022-12-06 22:36:41 +0900

    최종 완성본

---
+++ app/src/main/java/com/example/myapplication/ChatData.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+public class ChatData {
+    private String msg;
+    private String nickname;
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
