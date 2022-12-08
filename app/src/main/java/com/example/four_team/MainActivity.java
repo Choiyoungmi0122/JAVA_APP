2a5487a135da591fb44372acb2e220f0239f1c89
Choiyoungmi <choiyoungmi2252@gmail.com>
2022-12-09 00:08:06 +0900

    cor

--- app/src/main/java/com/example/four_team/MainActivity.java
+++ app/src/main/java/com/example/four_team/MainActivity.java
@@ -88,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//            .addOnCompleteListener(new OnCompleteListener<Void>() {
 //        @Override
 //        public void onComplete(@NonNull Task<Void> task) {
 //            if (task.isSuccessful()) {
